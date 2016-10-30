d4e2a9ab3d55fec357859231163059f7785c2ca5
Martin <mart.verdier@gmail.com>
2016-10-30 21:41:02 +0100

    Updated HTTP request, added field Well this still doesn't work. CORS in JS is a pain. Also it needs a real lift visually.

--- js/controllers/RedditController.js
+++ js/controllers/RedditController.js
@@ -1,10 +1,10 @@
 app.controller('MainController', ['$scope', '$http', function($scope, $http) {
 
 	$scope.getSubreddit = function(subreddit) {
-		var xmlHttp = new XMLHttpRequest();
-		xmlHttp.open("GET", theUrl, false);
-		xmlHttp.send(null);
-		$scope.content = xmlHttp.responseText;
+		$http.get("http://reddit.com/r/" + subreddit + ".json")
+		.then(function(response) {
+			$scope.content = response.data;
+		});
 	};
 
 }]);
